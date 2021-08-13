e8782740830941e28e6bf7b371726d7770b8e75c
Rostislav <rostisrost@gmail.com>
2021-08-13 20:08:30 +0300

    package gulp new file js css delete .txt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 document.body.innerHTML = '<h1>Hello,Git</h1>'
 document.body.innerHTML = '<h1>Hello,Git</h1>'
 console.log('test_8')
+
+document.body.innerHTML = '<h1>Hello,Git</h1>'
+document.body.innerHTML = '<h1>Hello,Git</h1>'
+console.log('test_8')
