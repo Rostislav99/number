2effb6242e124a34462a3158d204f0cbd4a00a9a
Rostislav99 <58180937+Rostislav99@users.noreply.github.com>
2021-08-13 20:11:09 +0300

    Update script.js
    js double

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ console.log('test_8')
 document.body.innerHTML = '<h1>Hello,Git</h1>'
 document.body.innerHTML = '<h1>Hello,Git</h1>'
 console.log('test_8')
+
+document.body.innerHTML = '<h1>Hello,Git</h1>'
+document.body.innerHTML = '<h1>Hello,Git</h1>'
+console.log('test_8')
+
+document.body.innerHTML = '<h1>Hello,Git</h1>'
+document.body.innerHTML = '<h1>Hello,Git</h1>'
+console.log('test_8')
